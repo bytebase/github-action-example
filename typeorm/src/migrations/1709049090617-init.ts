c1614edcdae1e10843c20cf2544219e43ec8ced8
Tianzhou Chen <t@bytebase.com>
2024-02-27 23:51:54 +0800

    chore: first init

---
+++ typeorm/src/migrations/1709049090617-init.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Change1709049090617 implements MigrationInterface {
+    name = 'Change1709049090617'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
