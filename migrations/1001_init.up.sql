56333588248c9e758caf310df2551ec1d52aaf04
tianzhou <t@bytebase.com>
2024-03-13 19:47:12 +0800

    chore: add NOT NULL

--- migrations/1001_init.up.sql
+++ migrations/1001_init.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE "user" (
   "id" SERIAL NOT NULL,
   "firstName" character varying NOT NULL,
   "lastName" character varying NOT NULL,
-  "age" integer,
+  "age" integer NOT NULL,
   CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
 );
