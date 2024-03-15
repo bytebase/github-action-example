11cc18453223e7d526733dd7cd5776fe693461a9
Tianzhou Chen <t@bytebase.com>
2024-03-16 01:51:39 +0800

    chore: change script

---
+++ migrations/1001_create_user.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "user" (
+  "id" SERIAL NOT NULL,
+  "firstName" character varying NOT NULL,
+  "lastName" character varying NOT NULL,
+  "age" integer NOT NULL,
+  CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+);
